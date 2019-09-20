d9cca1ce946710fc5f7c4610a638b174e2dd475b
mohamedagamy <mohamed.agamy1@vodafone.com>
2019-09-20 14:22:03 +0200

    Tax Safe

--- BLL/SafeService/SafeRepository.cs
+++ BLL/SafeService/SafeRepository.cs
@@ -1,7 +1,9 @@
 ﻿using BLL.RepositoryService;
 using DAL;
+using DAL.ConstString;
 using DAL.Entities;
 using DTO.SafeDataModel;
+using DTO.UserDataModel;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -23,18 +25,50 @@ namespace BLL.SafeService
 
         public decimal? GetCurrentAccount()
         {
-            return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 30 && w.Type == true) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 30 && w.Type == true).Sum(s => s.Amount) : 0)
-                - (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 100 && w.Type == false) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 100 && w.Type == false).Sum(s => s.Amount) : 0);
+            if (UserData.Role == RoleText.Admin)
+            {
+                return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Type == true) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Type == true).Sum(s => s.Amount) : 0)
+                       - (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Type == false) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Type == false).Sum(s => s.Amount) : 0);
+            }
+            else if (UserData.Role == RoleText.Tax)
+            {
+                return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 30 && w.Type == true) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 30 && w.Type == true).Sum(s => s.Amount) : 0)
+       - (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 100 && w.Type == false) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 100 && w.Type == false).Sum(s => s.Amount) : 0);
+            }
+            else
+                return null;
+
         }
 
         public int GetRecordsNumber(string key)
         {
-            return GeneralDBContext.Safes.AsNoTracking().Where(s => s.Amount <= 30 && (s.Statement + s.User.Name).Contains(key)).Count();
+            if (UserData.Role == RoleText.Admin)
+            {
+                return GeneralDBContext.Safes.AsNoTracking().Where(s => (s.Statement + s.User.Name).Contains(key)).Count();
+            }
+            else if (UserData.Role == RoleText.Tax)
+            {
+                return GeneralDBContext.Safes.AsNoTracking().Where(s => ((s.Amount <= 30 && s.Type == true) || (s.Amount <= 100 && s.Type == false)) && (s.Statement + s.User.Name).Contains(key)).Count();
+            }
+            else
+                return 0;
+
         }
 
         public int GetRecordsNumber(string key, DateTime dtFrom, DateTime dtTo)
         {
-            return GeneralDBContext.Safes.AsNoTracking().Where(s => s.Amount <= 30 && (s.Statement + s.User.Name).Contains(key) && s.RegistrationDate >= dtFrom && s.RegistrationDate <= dtTo).Count();
+            if (UserData.Role == RoleText.Admin)
+            {
+                return GeneralDBContext.Safes.AsNoTracking().Where(s => (s.Statement + s.User.Name).Contains(key) && s.RegistrationDate >= dtFrom && s.RegistrationDate <= dtTo).Count();
+
+            }
+            else if (UserData.Role == RoleText.Tax)
+            {
+                return GeneralDBContext.Safes.AsNoTracking().Where(s => ((s.Amount <= 30 && s.Type == true) || (s.Amount <= 100 && s.Type == false)) && (s.Statement + s.User.Name).Contains(key) && s.RegistrationDate >= dtFrom && s.RegistrationDate <= dtTo).Count();
+            }
+            else
+                return 0;
+
         }
 
         public List<string> GetStatementSuggetions()
@@ -44,30 +78,76 @@ namespace BLL.SafeService
 
         public decimal? GetTotalIncome(string key, DateTime dtFrom, DateTime dtTo)
         {
-            return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 30 && w.Type == true && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 30 && w.Type == true && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).Sum(s => s.Amount) : 0);
+            if (UserData.Role == RoleText.Admin)
+            {
+                return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Type == true && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Type == true && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).Sum(s => s.Amount) : 0);
+            }
+            else if (UserData.Role == RoleText.Tax)
+            {
+                return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 30 && w.Type == true && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 30 && w.Type == true && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).Sum(s => s.Amount) : 0);
+            }
+            else
+                return null;
+
         }
 
         public decimal? GetTotalOutgoings(string key, DateTime dtFrom, DateTime dtTo)
         {
-            return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 100 && w.Type == false && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 100 && w.Type == false && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).Sum(s => s.Amount) : 0);
+            if (UserData.Role == RoleText.Admin)
+            {
+                return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Type == false && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 100 && w.Type == false && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).Sum(s => s.Amount) : 0);
+            }
+            else if (UserData.Role == RoleText.Tax)
+            {
+                return (GeneralDBContext.Safes.AsNoTracking().Any(w => w.Amount <= 100 && w.Type == false && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo) ? GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 100 && w.Type == false && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).Sum(s => s.Amount) : 0);
+            }
+            else
+                return null;
+
         }
 
         public List<SafeDisplayDataModel> Search(string key, int pageNumber, int pageSize)
         {
-            return GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 30 && (w.Statement+ w.User.Name).Contains(key)).OrderByDescending(o => o.RegistrationDate).Skip((pageNumber - 1) * pageSize).Take(pageSize).Select(s => new SafeDisplayDataModel
+            if (UserData.Role == RoleText.Admin)
             {
-                Safe = s,
-                User = s.User
-            }).ToList();
+                return GeneralDBContext.Safes.AsNoTracking().Where(w => (w.Statement + w.User.Name).Contains(key)).OrderByDescending(o => o.RegistrationDate).Skip((pageNumber - 1) * pageSize).Take(pageSize).Select(s => new SafeDisplayDataModel
+                {
+                    Safe = s,
+                    User = s.User
+                }).ToList();
+            }
+            else if (UserData.Role == RoleText.Tax)
+            {
+                return GeneralDBContext.Safes.AsNoTracking().Where(w => ((w.Amount <= 30 && w.Type == true) || (w.Amount <= 100 && w.Type == false)) && (w.Statement + w.User.Name).Contains(key)).OrderByDescending(o => o.RegistrationDate).Skip((pageNumber - 1) * pageSize).Take(pageSize).Select(s => new SafeDisplayDataModel
+                {
+                    Safe = s,
+                    User = s.User
+                }).ToList();
+            }
+            else
+                return null;
         }
 
         public List<SafeDisplayDataModel> Search(string key, DateTime dtFrom, DateTime dtTo, int pageNumber, int pageSize)
         {
-            return GeneralDBContext.Safes.AsNoTracking().Where(w => w.Amount <= 30 && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).OrderByDescending(o => o.RegistrationDate).Skip((pageNumber - 1) * pageSize).Take(pageSize).Select(s => new SafeDisplayDataModel
+            if (UserData.Role == RoleText.Admin)
             {
-                Safe = s,
-                User = s.User
-            }).ToList();
+                return GeneralDBContext.Safes.AsNoTracking().Where(w => (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).OrderByDescending(o => o.RegistrationDate).Skip((pageNumber - 1) * pageSize).Take(pageSize).Select(s => new SafeDisplayDataModel
+                {
+                    Safe = s,
+                    User = s.User
+                }).ToList();
+            }
+            else if (UserData.Role == RoleText.Tax)
+            {
+                return GeneralDBContext.Safes.AsNoTracking().Where(w => ((w.Amount <= 30 && w.Type == true) || (w.Amount <= 100 && w.Type == false)) && (w.Statement + w.User.Name).Contains(key) && w.RegistrationDate >= dtFrom && w.RegistrationDate <= dtTo).OrderByDescending(o => o.RegistrationDate).Skip((pageNumber - 1) * pageSize).Take(pageSize).Select(s => new SafeDisplayDataModel
+                {
+                    Safe = s,
+                    User = s.User
+                }).ToList();
+            }
+            else
+                return null;
         }
     }
 }
